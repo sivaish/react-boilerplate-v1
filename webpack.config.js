e096edab6841e66a8c3af9eb6a9fb5be0883e828
sivaish <sivachidambaram22@gmail.com>
2019-09-19 21:42:40 +1000

    react boilerplate initial commit version1

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'boilerplate-webpack-bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader' ,
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        // Proxy for server - side code
+        historyApiFallback: true
+      }
+}
